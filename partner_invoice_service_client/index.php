f55684113c4671a345ec50fed1d21daf2b14b34f
LieAlbertTriAdrian <alberttri23@gmail.com>
2016-07-21 12:20:40 +0700

    add xendit v2 invoice client

--- partner_invoice_service_client/index.php
+++ partner_invoice_service_client/index.php
@@ -1,73 +1,71 @@
 <?php
    class PartnerInvoiceServiceClient {
       
-      function __construct ($options) {
-         $this->server_domain = $options['server_domain'];
-         $this->partner_id = $options['partner_id'];
-         $this->secret_key = $options['secret_key'];
-      }
+        function __construct ($options) {
+            $this->server_domain = $options['server_domain'];
+            $this->partner_id = $options['partner_id'];
+            $this->secret_key = $options['secret_key'];
+        }
 
-      function getInvoice ($invoice_id, $nonce, $signature) {
-         $curl = curl_init();
+        function getInvoice ($invoice_id, $nonce, $signature) {
+            $curl = curl_init();
 
-         $end_point = $this->server_domain.'/invoices/'.$invoice_id.'?partner_id='.$this->partner_id.'&nonce='.$nonce.'&signature='.$signature;
+            $end_point = $this->server_domain.'/invoices/'.$invoice_id.'?partner_id='.$this->partner_id.'&nonce='.$nonce.'&signature='.$signature;
 
-         echo $end_point;
+            curl_setopt($curl, CURLOPT_URL, $end_point); 
 
-         curl_setopt($curl, CURLOPT_URL, $end_point); 
+            curl_exec($curl);         
+            curl_close($curl);
+        }
 
-         curl_exec($curl);         
-         curl_close($curl);
-      }
+        function postInvoice ($amount, $payer_email, $description) {
+            $curl = curl_init();
 
-      function postInvoice ($amount, $payer_email, $description) {
-         $curl = curl_init();
+            $end_point = $this->server_domain.'/invoices';
 
-      	$end_point = $this->server_domain.'/invoices';
+            $data["nonce"] = date('Y-m-d\TH:i:s\Z');
+            $data["partner_id"] = $this->partner_id;
+            $data["amount"] = $amount;
+            $data["payer_email"] = $payer_email;
+            $data["description"] = $description;
+            $data["signature"] = $this->generateSignature($data, $this->secret_key);
 
-         $data["nonce"] = date('Y-m-d\TH:i:s\Z');
-         $data["partner_id"] = $this->partner_id;
-         $data["amount"] = $amount;
-         $data["payer_email"] = $payer_email;
-         $data["description"] = $description;
-         $data["signature"] = $this->generateSignature($data, $this->secret_key);
+            $raw_data = $this->generateRawData($data);
 
-         $raw_data = $this->generateRawData($data);
+            curl_setopt($curl, CURLOPT_URL, $end_point); 
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $raw_data);
 
-         curl_setopt($curl, CURLOPT_URL, $end_point); 
-         curl_setopt($curl, CURLOPT_POST, true);
-         curl_setopt($curl, CURLOPT_POSTFIELDS, $raw_data);
+            curl_exec($curl);
+            curl_close($curl);
+        }
 
-         curl_exec($curl);
-         curl_close($curl);
-      }
+        function generateSignature ($data, $key) {
+            $value = "";
 
-		function generateSignature ($data, $key) {
-		    $value = "";
-
-		    foreach ($data as $datum) {
-		    	$value .= $datum;
-		    }
-
-		    return $this->str2hex(hash_hmac("sha1", $value, $key, TRUE));
-		}
-
-		function generateRawData ($data) {
-         $raw_data = "";
-
-         foreach($data as $key => $value) {
-            if ($key === 'signature') {
-               $raw_data .= $key . "=" . $value;
-            } else {
-               $raw_data .= $key . "=" . $value . "&";               
+            foreach ($data as $datum) {
+                $value .= $datum;
             }
-         }
 
-         return $raw_data;
-		}
+            return $this->str2hex(hash_hmac("sha1", $value, $key, TRUE));
+        }
 
-		function str2hex ($str) {
-			return array_shift(unpack('H*', $str));
-		}
-	}
+        function generateRawData ($data) {
+            $raw_data = "";
+
+            foreach($data as $key => $value) {
+                if ($key === 'signature') {
+                   $raw_data .= $key . "=" . $value;
+                } else {
+                   $raw_data .= $key . "=" . $value . "&";               
+                }
+            }
+
+            return $raw_data;
+        }
+
+        function str2hex ($str) {
+            return array_shift(unpack('H*', $str));
+        }
+    }
 ?>
---
+++ xendit_v2_invoice_client/examples/create_invoice_example.php
@@ -0,0 +1,17 @@
+<?php
+	require('../config/xendit_v2_invoice_client_config.php');
+	require('../../config/secret_key.php');
+	require('../index.php');
+
+	$options['server_domain'] = constant('SERVER_DOMAIN');
+	$options['secret_api_key'] = constant('SECRET_API_KEY');
+
+	$xenditV2InvoiceClient = new XenditV2InvoiceClient($options);
+
+	$external_id = $argv[1];
+	$amount = $argv[2];
+	$payer_email = $argv[3];
+	$description = $argv[4];
+
+	$xenditV2InvoiceClient->createInvoice($external_id, $amount, $payer_email, $description);
+?>
