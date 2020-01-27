69641ff015116cb1864c13cf816b17d1837b3c3d
unknown <ellennugroho9@gmail.com>
2020-01-27 13:45:20 +0700

    added PHP client usage examples for cards

--- examples/CardsReverseExample.php
+++ examples/CardsReverseExample.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * CardsExample.php
+ * CardsReverseExample.php
  * php version 7.2.0
  *
  * @category Example
@@ -24,34 +24,19 @@ TAG
 $params = [
     'token_id' => '5e2e8231d97c174c58bcf644',
     'external_id' => 'card_' . time(),
-    'authentication_id' => '5e2e8658bae82e4d54d764c0',
+    'authentication_id' => '5e2e85f9bae82e4d54d764b5',
     'amount'=> 100000,
     'card_cvn'=>'123',
     'capture'=> false
 ];
 
-$captureParams = ['amount' => 100000];
-
-$refundParams = [
-    'external_id' => $params['external_id'],
-    'amount' => 20000
-];
-
 $createCharge = \Xendit\Cards::create($params);
 var_dump($createCharge);
 
-$id = $createCharge['id'];
+$reverseParams = ['external_id' => $createCharge['external_id']];
 
-$getCharge = \Xendit\Cards::retrieve($id);
-var_dump($getCharge);
-
-$captureCharge = \Xendit\Cards::capture($id, $captureParams);
-var_dump($captureParams);
-
-$getCharge = \Xendit\Cards::retrieve($id);
-var_dump($getCharge);
-
-$refund = \Xendit\Cards::createRefund($id, $refundParams);
-var_dump($refund);
-
-
+$reverseAuth = \Xendit\Cards::reverseAuthorization(
+    $createCharge['id'],
+    $reverseParams
+);
+var_dump($reverseAuth);
