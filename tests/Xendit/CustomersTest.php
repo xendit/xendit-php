3775b90537adf8c45c91eeea4988105bd6056596
Glenda Emanuella Sutanto <glendaes0103@gmail.com>
2021-07-30 11:33:03 +0700

    Add create customer with header test

--- tests/Xendit/CustomersTest.php
+++ tests/Xendit/CustomersTest.php
@@ -106,6 +106,85 @@ class CustomersTest extends TestCase
     }
 
     /**
+     * Create customer test with headers
+     * Should pass
+     *
+     * @return void
+     * @throws Exceptions\ApiException
+     */
+    public function testIsCustomerCreatableWithHeaders()
+    {
+        $params = [
+            'reference_id' => 'test-ref-iddd',
+            'given_names' => 'customer 1',
+            'email' => 'customer@website.com',
+            'mobile_number' => '+6281212345678',
+            'description' => 'dummy customer',
+            'middle_name' => 'middle',
+            'surname' => 'surname',
+            'addresses' => [
+                [
+                    'country' => 'ID',
+                    'street_line1' => 'Jl. 123',
+                    'street_line2' => 'Jl. 456',
+                    'city' => 'Jakarta Selatan',
+                    'province' => 'DKI Jakarta',
+                    'state' => '-',
+                    'postal_code' => '12345'
+                ]
+            ],
+            'metadata' => [
+                'meta' => 'data'
+            ],
+            'for-user-id' => 'user-id'
+        ];
+
+        $response = [
+            'id' => '0f2de6f1-2023-403b-aaea-5f0a8a611f7d',
+            'reference_id' => 'test-ref-iddd',
+            'given_names' => 'customer 1',
+            'email' => 'customer@website.com',
+            'mobile_number' => '+6281212345678',
+            'description' => 'dummy customer',
+            'middle_name' => 'middle',
+            'surname' => 'surname',
+            'phone_number' => null,
+            'nationality' => null,
+            'date_of_birth' => null,
+            'metadata' => [
+                'meta' => 'data'
+            ],
+            'employment' => null,
+            'addresses' => [
+                [
+                    'category' => '',
+                    'country' => 'ID',
+                    'state' => '-',
+                    'province' => 'DKI Jakarta',
+                    'city' => 'Jakarta Selatan',
+                    'postal_code' => '12345',
+                    'street_line1' => 'Jl. 123',
+                    'street_line2' => 'Jl. 456',
+                    'is_preferred' => false
+                ]
+            ],
+            'source_of_wealth' => null
+        ];
+
+        $this->stubRequest(
+            'POST',
+            '/customers',
+            $params,
+            [],
+            $response
+        );
+
+        $result = Customers::createCustomer($params);
+
+        $this->assertEquals($response, $result);
+    }
+
+    /**
      * Create customer test
      * Should throw InvalidArgumentException
      *
