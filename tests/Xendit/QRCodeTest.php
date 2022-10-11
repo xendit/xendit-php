76bc18e9795c5516a316aeb6c20b15c5f3c131d5
Ainun Nazieb <ainun@nazieb.com>
2022-10-12 01:16:38 +0700

    feat: support QR Code with API Version 2022-07-31

--- tests/Xendit/QRCodeTest.php
+++ tests/Xendit/QRCodeTest.php
@@ -3,7 +3,7 @@
 /**
  * QRCodeTest.php
  * php version 7.2.0
- * 
+ *
  * @category Test
  * @package  Xendit
  * @author   Dave <kevindave@xendit.co>
@@ -17,7 +17,7 @@ use Xendit\QRCode;
 
 /**
  * Class QRCodeTest
- * 
+ *
  * @category Class
  * @package  Xendit
  * @author   Dave <kevindave@xendit.co>
@@ -31,7 +31,7 @@ class QRCodeTest extends TestCase
   /**
    * Create QR Code test
    * Should pass
-   * 
+   *
    * @return void
    */
   public function testIsCreatable()
@@ -55,10 +55,12 @@ class QRCodeTest extends TestCase
       'updated' => '2020-01-08T18:18:18.661Z',
     ];
 
+    $expectedParams = array_merge($params, ['api-version' => '2020-07-01']);
+
     $this -> stubRequest(
       'POST',
       '/qr_codes',
-      $params,
+      $expectedParams,
       [],
       $expectedResult
     );
@@ -76,10 +78,61 @@ class QRCodeTest extends TestCase
     $this->assertEquals($result['updated'], '2020-01-08T18:18:18.661Z');
   }
 
+    /**
+     * Create V2 QR Code test
+     * Should pass
+     *
+     * @return void
+     */
+    public function testIsCreatableV2()
+    {
+        $params = [
+            'api_version' => '2022-07-31',
+            'reference_id' => self::TEST_EXTERNAL_ID,
+            'currency' => 'IDR',
+            'type' => 'DYNAMIC',
+            'amount' => 10000,
+        ];
+
+        $expectedResult = [
+            'id' => 'id1',
+            'reference_id' => self::TEST_EXTERNAL_ID,
+            'currency' => 'IDR',
+            'amount' => 10000,
+            'qr_string' => 'this_is_qr_string',
+            'type' => 'DYNAMIC',
+            'status' => 'ACTIVE',
+            'created' => '2020-01-08T18:18:18.661Z',
+            'updated' => '2020-01-08T18:18:18.661Z',
+        ];
+
+        $expectedParams = array_merge($params, ['api-version' => '2022-07-31']);
+        unset($expectedParams['api_version']);
+
+        $this -> stubRequest(
+            'POST',
+            '/qr_codes',
+            $expectedParams,
+            [],
+            $expectedResult
+        );
+
+        $result = QRCode::create($params);
+
+        $this->assertEquals($result['id'], 'id1');
+        $this->assertEquals($result['reference_id'], $params['reference_id']);
+        $this->assertEquals($result['amount'], $params['amount']);
+        $this->assertEquals($result['qr_string'], 'this_is_qr_string');
+        $this->assertEquals($result['type'], $params['type']);
+        $this->assertEquals($result['status'], 'ACTIVE');
+        $this->assertEquals($result['created'], '2020-01-08T18:18:18.661Z');
+        $this->assertEquals($result['updated'], '2020-01-08T18:18:18.661Z');
+    }
+
   /**
    * Create QR Code
    * Should throw InvalidArgumentException with type not exist
-   * 
+   *
    * @return void
    */
   public function testIsCreatableThrowInvalidArgumentExceptionIfTypeDoesntExist()
@@ -93,7 +146,7 @@ class QRCodeTest extends TestCase
   /**
    * Create QR Code
    * Should throw InvalidArgumentExcpetion with type not exist
-   * 
+   *
    * @return void
    */
   public function testIsCreatableThrowInvalidArgumentExceptionOnWrongType()
@@ -106,10 +159,69 @@ class QRCodeTest extends TestCase
     QRCode::create($params);
   }
 
+    /**
+     * Create QR Code
+     * Should throw InvalidArgumentException with wrong API version
+     *
+     * @return void
+     */
+    public function testIsCreatableThrowInvalidArgumentExceptionOnWrongAPIVersion()
+    {
+        $this->expectException(Exceptions\InvalidArgumentException::class);
+        $params = [
+            'external_id' => self::TEST_EXTERNAL_ID,
+            'type' => 'DYNAMIC',
+            'callback_url' => 'https://webhook.site',
+            'amount' => 10000,
+            'api_version' => '2022-02-02'
+        ];
+
+        QRCode::create($params);
+    }
+
+    /**
+     * Create QR Code
+     * Should throw InvalidArgumentException with wrong parameters for V2
+     *
+     * @return void
+     */
+    public function testIsCreatableThrowInvalidArgumentExceptionOnMissingReferenceIDForV2()
+    {
+        $this->expectException(Exceptions\InvalidArgumentException::class);
+        $params = [
+            'external_id' => self::TEST_EXTERNAL_ID, // expects this to be reference_id instead
+            'type' => 'DYNAMIC',
+            'amount' => 10000,
+            'api_version' => '2022-07-31'
+        ];
+
+        QRCode::create($params);
+    }
+
+    /**
+     * Create QR Code
+     * Should throw InvalidArgumentException if callback URL is provided for V2
+     *
+     * @return void
+     */
+    public function testIsCreatableThrowInvalidArgumentExceptionOnCallbackURLForV2()
+    {
+        $this->expectException(Exceptions\InvalidArgumentException::class);
+        $params = [
+            'reference_id' => self::TEST_EXTERNAL_ID,
+            'type' => 'DYNAMIC',
+            'amount' => 10000,
+            'callback_url' => 'https://webhook.site',
+            'api_version' => '2022-07-31'
+        ];
+
+        QRCode::create($params);
+    }
+
   /**
    * Get QR Code test
    * Should pass
-   * 
+   *
    * @return void
    */
   public function testIsGettable()
